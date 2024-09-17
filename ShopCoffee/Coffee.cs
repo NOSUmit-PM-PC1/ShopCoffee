9e6120f1574cfb4d90158f066effcf0253ecc9d6
Мария <mahadrum@yandex.ru>
2024-09-17 09:34:29 +0300

    Добавьте файлы проекта.

---
+++ ShopCoffee/Coffee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCoffee
+{
+    class TypeProduct
+    {
+        public string Name { get; }
+
+        public TypeProduct(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class Product
+    {
+        public string Title { get; }
+        public decimal Cost { get; }
+        public TypeProduct TypeProduct { get; }
+
+        public Product(string title, decimal cost, TypeProduct typeProduct)
+        {
+            this.Title = title;
+            this.Cost = cost;
+            this.TypeProduct = typeProduct;
+        }
+
+    }
+}
---
+++ ShopCoffee/FormCatalog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopCoffee
+{
+    public partial class FormCatalog : Form
+    {
+        public FormCatalog()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TypeProduct t = new TypeProduct("кофе");
+            Product pr1 = new Product("эспрессо", 100, t);
+            label1.Text = pr1.Title + " " + pr1.Cost + " " + pr1.TypeProduct;
+        }
+    }
+}
